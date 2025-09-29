03a204bd0cfa539ede5c01bbd5853f4577bd0c7b
MelsonHansMascarenhas <www.melsonhans003@gmail.com>
2025-09-29 15:03:27 +0530

    Create Eligibility.py
    Eligible

---
+++ Eligibility.py
@@ -0,0 +1,11 @@
+# vote_eligibility.py
+# Get age input from the user
+try:
+    age = int(input("Enter your age: "))
+    # Check eligibility
+    if age >= 18:
+        print("You are eligible to vote.")
+    else:
+        print("You are not eligible to vote.")
+except ValueError:
+    print("Please enter a valid number.")
